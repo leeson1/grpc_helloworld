54a91e7796f343d044c18df9813394ba7271b670
root <root@DESKTOP-5PCLA8D.localdomain>
2023-08-09 18:16:35 +0800

    add .gitignore

--- src/main.cpp
+++ src/main.cpp
@@ -13,45 +13,47 @@
 // #include "src/thing.grpc.pb.h"
 #include "proto/hello.grpc.pb.h"
 
-
 using grpc::Server;
 using grpc::ServerBuilder;
 using grpc::ServerContext;
 using grpc::Status;
 
-class GreeterServerImpl final :public Greeter::Service {
+class GreeterServerImpl final : public Greeter::Service
+{
   grpc::Status SayHello(ServerContext* context, const HelloRequest* request,
-                  HelloReply* reply) override {
+                        HelloReply* reply) override
+  {
     std::string prefix("Hello ");
     reply->set_message(prefix + request->name());
     return Status::OK;
   }
 
   grpc::Status SayHelloAgain(ServerContext* context, const HelloRequest* request,
-                       HelloReply* reply) override {
+                             HelloReply* reply) override
+  {
     std::string prefix("Hello again ");
     reply->set_message(prefix + request->name());
     return Status::OK;
   }
 };
 
+void RunServer(uint16_t port)
+{
+  std::string server_address = absl::StrFormat("0.0.0.0:%d", port);
+  GreeterServerImpl service;
 
-void RunServer(uint16_t port) {
-    std::string server_address = absl::StrFormat("0.0.0.0:%d",port);
-    GreeterServerImpl service;
+  grpc::EnableDefaultHealthCheckService(true);
+  grpc::reflection::InitProtoReflectionServerBuilderPlugin();
 
-    grpc::EnableDefaultHealthCheckService(true);
-    grpc::reflection::InitProtoReflectionServerBuilderPlugin();
+  ServerBuilder builder;
+  builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+  builder.RegisterService(&service);
 
-    ServerBuilder builder;
-    builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
-    builder.RegisterService(&service);
-
-    std::unique_ptr<Server> server(builder.BuildAndStart());
-    
+  std::unique_ptr<Server> server(builder.BuildAndStart());
 }
 
-int main() {
-    
+int main()
+{
+  
 
 }
