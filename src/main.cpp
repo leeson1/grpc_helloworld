28f10d58399581f400d8a3473a8c2f0babcc781e
root <root@DESKTOP-5PCLA8D.localdomain>
2023-08-10 16:32:40 +0800

    add server

--- src/main.cpp
+++ src/main.cpp
@@ -10,50 +10,50 @@
 #include <grpcpp/grpcpp.h>
 #include <grpcpp/health_check_service_interface.h>
 
-// #include "src/thing.grpc.pb.h"
-#include "proto/hello.grpc.pb.h"
+#include "proto/helloworld.grpc.pb.h"
 
 using grpc::Server;
 using grpc::ServerBuilder;
 using grpc::ServerContext;
 using grpc::Status;
+using helloworld::HelloRequest;
+using helloworld::HelloReply;
+using helloworld::Greeter;
 
-class GreeterServerImpl final : public Greeter::Service
-{
-  grpc::Status SayHello(ServerContext* context, const HelloRequest* request,
-                        HelloReply* reply) override
-  {
+class GreeterServiceImpl final : public Greeter::Service {
+  Status SayHello(ServerContext* context, const HelloRequest* request,
+                  HelloReply* reply) override {
     std::string prefix("Hello ");
     reply->set_message(prefix + request->name());
     return Status::OK;
   }
 
-  grpc::Status SayHelloAgain(ServerContext* context, const HelloRequest* request,
-                             HelloReply* reply) override
-  {
+  Status SayHelloAgain(ServerContext* context, const HelloRequest* request,
+                       HelloReply* reply) override {
     std::string prefix("Hello again ");
     reply->set_message(prefix + request->name());
     return Status::OK;
   }
 };
 
-void RunServer(uint16_t port)
-{
-  std::string server_address = absl::StrFormat("0.0.0.0:%d", port);
-  GreeterServerImpl service;
-
-  grpc::EnableDefaultHealthCheckService(true);
-  grpc::reflection::InitProtoReflectionServerBuilderPlugin();
+void RunServer() {
+  std::string address = "0.0.0.0";
+  std::string port = "5001";
+  std::string server_address = address + ":" + port;
+  GreeterServiceImpl service;
 
   ServerBuilder builder;
   builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
-  builder.RegisterService(&service);
 
+  builder.RegisterService(&service);
   std::unique_ptr<Server> server(builder.BuildAndStart());
+  std::cout << "Server listening on " << server_address << std::endl;
+
+
+  server->Wait();
 }
 
-int main()
-{
-  
-
+int main(int argc, char** argv) {
+  RunServer();
+  return 0;
 }
